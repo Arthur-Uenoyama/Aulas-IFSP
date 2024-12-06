cbcd94029ab78356da99d277cfb14c60a5acae49
Arthur Uenoyama Dezem <uenoarthur@outlook.com>
2024-12-06 14:41:55 -0300

    Delete Aulas/Aula2PHP directory

--- Aulas/Aula2PHP/carrinho.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $pg_atual = 'carrinho';
-  include('menu.php'); 
-?>
-<h1>Página do Carrinho</h1>
-<p>Aqui estão os itens do seu carrinho.</p>
--- Aulas/Aula2PHP/produtos.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $pg_atual = 'produtos';
-  include('menu.php'); 
-?>
-<h1>Página de Produtos</h1>
-<p>Aqui estão os produtos disponíveis.</p>
